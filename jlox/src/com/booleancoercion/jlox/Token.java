76ae6ab03acdbc65df3fc3cdd0d7b313660b3c82
boolean_coercion <booleancoercion@gmail.com>
2021-06-24 23:28:17 +0300

    Implemented lexer for jlox

---
+++ jlox/src/com/booleancoercion/jlox/Token.java
@@ -0,0 +1,19 @@
+package com.booleancoercion.jlox;
+
+public class Token {
+	public final TokenType type;
+	public final String lexeme;
+	public final Object literal;
+	public final int line;
+	
+	Token(TokenType type, String lexeme, Object literal, int line) {
+		this.type = type;
+		this.lexeme = lexeme;
+		this.literal = literal;
+		this.line = line;
+	}
+	
+	public String toString() {
+		return String.format("%s %s %s", type, lexeme, literal);
+	}
+}
